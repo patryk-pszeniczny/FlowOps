ac6c9070b0ecb6628c16011ea6be2578a59136c1
Patryk Pszeniczny <vcode1@onet.pl>
2025-11-17 15:39:10 +0100

    Added in-memory subscription repository
    Introduced the `InMemorySubscriptionRepository` class implementing the `ISubscriptionRepository` interface. The class uses a `ConcurrentDictionary` to store subscriptions in memory. Added the `Add` method for inserting subscriptions and the `TryGet` method for retrieving them. Handled identifier conflicts by throwing an `InvalidOperationException`.

---
+++ FlowOps/Domain/Subscriptions/InMemorySubscriptionRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+
+namespace FlowOps.Domain.Subscriptions
+{
+    public class InMemorySubscriptionRepository : ISubscriptionRepository
+    {
+        private readonly ConcurrentDictionary<Guid, Subscription> _store = new();
+        
+        public void Add(Subscription subscription)
+        {
+            if(!_store.TryAdd(subscription.Id, subscription))
+            {
+                throw new InvalidOperationException($"Subscription with ID {subscription.Id} already exists.");
+            }
+        }
+        public bool TryGet(Guid id, out Subscription? subscription)
+            => _store.TryGetValue(id, out subscription);
+    }
+}
