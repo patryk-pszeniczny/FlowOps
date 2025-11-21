eddf39dba72948d1cc899a38fb94225d091ec06f
Patryk Pszeniczny <vcode1@onet.pl>
2025-11-21 13:48:51 +0100

    Add SubscriptionResumedEvent and SubscriptionSuspendedEvent
    Introduce two new event classes, `SubscriptionResumedEvent` and
    `SubscriptionSuspendedEvent`, in the `FlowOps.Events` namespace.
    Both classes inherit from the `IntegrationEvent` base class and
    include properties for `SubscriptionId`, `CustomerId`, and
    `PlanCode`. These events are designed to represent subscription
    resumption and suspension in an event-driven architecture.

---
+++ FlowOps/Events/SubscriptionSuspendedEvent.cs
@@ -0,0 +1,11 @@
+﻿using FlowOps.BuildingBlocks.Integration;
+
+namespace FlowOps.Events
+{
+    public class SubscriptionSuspendedEvent : IntegrationEvent
+    {
+        public Guid SubscriptionId { get; init; }
+        public Guid CustomerId { get; init; }
+        public string PlanCode { get; init; } = string.Empty;
+    }
+}
